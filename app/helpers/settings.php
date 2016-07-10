4eb99e7b227f6a380a923237702b9ec53078f23f
Sammy Griffiths <contact@sammygriffiths.co.uk>
2016-07-10 20:14:52 +0100

    Tweak how settings are handled

--- app/helpers/settings.php
+++ app/helpers/settings.php
@@ -12,14 +12,20 @@ class Settings
     }
 
     public function all() {
-        return $this->settings;
+        return isset($this->settings) ? $this->settings : null;
     }
 
     public function get($setting) {
-        return $this->settings->$setting;
+        return isset($this->settings->$setting) ? $this->settings->$setting : null;
     }
 
     public function service($service, $setting) {
-        return $this->settings->services->$service->$setting;
+        if (isset($this->settings->services->$service->$setting)) {
+            return $this->settings->services->$service->$setting;
+        } elseif (null !== $this->get($setting)) {
+            return $this->get($setting);
+        } else {
+            return null;
+        }
     }
 }
